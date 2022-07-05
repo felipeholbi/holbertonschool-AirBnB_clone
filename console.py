3411bec3e33da733b1c5fd3b9d47e2423bd93740
felipeholbi <4758@holbertonshcool.com>
2022-07-04 20:54:47 -0500

    console_0.0.1

--- console.py
+++ console.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+""" this module is the command interpreter"""
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """command interpreter"""
+    """develop methods"""
     prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        """EOF: to exit the program"""
+        return True
+    def do_quit(self, line):
+        """quit: to exit the program"""
+        return True
+    def emptyline(self):
+        """any action"""
+        pass
+    def main():
+        """Don't executed when imported"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
