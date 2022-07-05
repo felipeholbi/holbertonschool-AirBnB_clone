c898a5b2682df14d1804499e404855e09b525438
felipeholbi <4758@holbertonshcool.com>
2022-07-04 21:06:11 -0500

    fix_console_6

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ this module is the command interpreter"""
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """develop methods"""
     prompt = '(hbnb)'
@@ -9,15 +10,19 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """EOF: to exit the program"""
         return True
+
     def do_quit(self, line):
         """quit: to exit the program"""
         return True
+
     def emptyline(self):
         """any action"""
         pass
+
     def main():
         """Don't executed when imported"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
