fa6de5a0e97d614f0c1b119cbe510fc67ab65cbd
alejuran <alejandrourn@gmail.com>
2022-07-06 15:46:22 -0500

    update style city

--- models/city.py
+++ models/city.py
@@ -2,10 +2,9 @@
 """ class City """
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
-    """
-    ...
-    """
+    """class City that inherits from BaseModel"""
     state_id = ""
     name = ""
 
