e84455b10a989612dae978b94fd795c83eeaaf39
felipeholbi <4758@holbertonshcool.com>
2022-07-06 13:21:47 -0500

    all_methods

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.review import Review
 from models.user import User
 from models.state import State
 from models import storage
+import shlex
 
 classe = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
           "Place": Place, "Review": Review, "State": State, "User": User}
@@ -18,7 +19,6 @@ classe = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
 class HBNBCommand(cmd.Cmd):
     """develop methods"""
     prompt = '(hbnb) '
-
 
     def do_EOF(self, line):
         """EOF: to exit the program"""
@@ -66,7 +66,7 @@ class HBNBCommand(cmd.Cmd):
                         print("** no instance found **")
             else:
                 print("** class doesn't exist **")
-    
+
     def do_destroy(self, line):
         tokens = line.split()
         if len(tokens) == 0:
@@ -88,10 +88,54 @@ class HBNBCommand(cmd.Cmd):
                         print("** no instance found **")
             else:
                 print("** class doesn't exist **")
-    
-    def do_all(self, line):
-        if line not in HBNBCommand.classe:
+
+    def do_all(self, line=""):
+        """prints all string representation of all instances"""
+        objects = storage.all()
+        list_objects = list()
+        if line == "":
+            for value in objects.values():
+                list_objects.append(str(value))
+            print(list_objects)
+        elif line in classe.keys():
+            for key, value in objects.items():
+                if line in key:
+                    list_objects.append(str(value))
+            print(list_objects)
+        else:
             print("** class doesn't exist **")
-    
+
+    def do_update(self, line):
+        """update an instance based on the class id"""
+        tokens = shlex.split(line)
+        integers = ['number_rooms', 'number_bathrooms',
+                    'max_guest', 'price_by_night']
+        floats = ['latitude', 'longitude']
+        if len(tokens) == 0:
+            print("** class name missing **")
+        elif len(tokens) == 1 and tokens[0] in self.classes.keys():
+            print("** instance id missing **")
+        elif tokens[0] not in classe.keys():
+            print("** class doesn't exist **")
+        elif len(tokens) == 2:
+            print("** attribute name missing **")
+        elif len(tokens) == 3:
+            print("** value missing **")
+        else:
+            objects = storage.all()
+            flag = 0
+            for k in objects.keys():
+                if str(tokens[1]) in k:
+                    if tokens[2] in integers:
+                        tokens[3] = int(tokens[3])
+                    elif tokens[2] in floats:
+                        tokens[3] = float(tokens[3])
+                    setattr(objects[k], tokens[2], tokens[3])
+                    objects[k].save()
+                    flag = 1
+            if flag == 0:
+                print("** no instance found **")
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
