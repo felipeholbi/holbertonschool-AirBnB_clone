6f41fef4d425e5bf5a65bc2b8583468ee165b6b5
alejuran <alejandrourn@gmail.com>
2022-07-07 07:55:43 -0500

    test amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+"""unittest for amenity"""
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+import os
 
+
+class testAmenity(unittest.TestCase):
+    """test Amenity class"""
+
+    def setUp(self):
+        """test Set method"""
+        pass
+
+    def test_attr(self):
+        """test attributes correct types"""
+        self.assertIs(self.amenity.name, st())
+
+    def test_str(self):
+        """test str method"""
+        amenity = Amenity()
+        string = "[Amenity]({}){}".format(amenity.id, amenity.__dict__)
+        self.assertEqual(string, str(amenity))
+
+
+if __name__ == "__main__":
+    unittest.main()
