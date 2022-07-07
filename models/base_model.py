9c4a257e04edae290908cb5d3da7c6f2746b5c29
felipeholbi <4758@holbertonshcool.com>
2022-07-06 22:45:30 -0500

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -16,16 +16,17 @@ class BaseModel:
         """ Initialize BaseModel class """
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at":
+                if key == 'created_at':
                     self.created_at = datetime.strptime(value, date_time)
-                elif key == "updated_at":
+                elif key == 'updated_at':
                     self.updated_at = datetime.strptime(value, date_time)
-                elif key != "__class__":
-                    setattr(self, key, value)
+                else:
+                    if key != "__class__":
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.update_at = self.created_at
+            self.update_at = datetime.now()
             models.storage.new(self)
 
     def save(self):
@@ -34,12 +35,16 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        """Returns a new dict of the class"""
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = type(self).__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        copy_dict = self.__dict__.copy()
+        copy_dict["__class__"] = type(self).__name__
+        for key, value in copy_dict.items():
+            if isinstance(value, datetime):
+                copy_dict[key] = value.isoformat()
+        return copy_dict
 
     def __str__(self):
         """ Returns a string BaseModel class """
